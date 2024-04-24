96a21e73f26fc2f4dbe9885475dd7e2647d2ccb1
JuanSeSaCa <156488290+JuanSeSaCa@users.noreply.github.com>
2024-04-24 13:02:20 -0500

    feat: :construction: anaces requerimientos 5

--- js/module/employee.js
+++ js/module/employee.js
@@ -1,10 +1,10 @@
 // 3.Devuelve un listado con el nombre, apellidos y email 
 //de los empleados cuyo jefe tiene un código de jefe igual a 7.
-
+/*
 export async function getAllFullNameAndEmails (codeBoss) {
-   const res = await fetch ("http://localhost:3000/employee?code_boss=7")
-     let data = await res.json();
-     let dataUpdate = data.map(employee=>{
+   const res = await fetch (`http://localhost:3000/employee?code_boss=${codeBoss}`)
+     let employee  = await res.json();
+     let dataUpdate = employee.map(employee=>{
 
          return {
              name: employee.name,
@@ -13,19 +13,37 @@ export async function getAllFullNameAndEmails (codeBoss) {
          }
      })
 
-     console.log(dataUpdate);
-
-     
+     console.log(dataUpdate);   
  }
+ getAllFullNameAndEmails(7)
+*/
 
- getAllFullNameAndEmails()
+
 
 
 //4. devuelve  el nombre del puesto, nombre, apellidos y 
 //email del jefe de la empresa  
 
 // export const getBoss = async () =>{
-//     let res = await fetch ("http://localhost:5502/employees")
-//     let data = await res.json();
+//      let res = await fetch ("http://localhost:5502/employees")
+//      let data = await res.json();
 
-//     let boss = data.find(val=> val.boss === true);
+//      let boss = data.find(val=> val.boss === true);
+
+// export async function getBoss(codeBoss=null) {
+//         const res = await fetch (`http://localhost:3000/employee?code_boss===${codeBoss}`)
+//           let employee  = await res.json();
+//           let dataUpdate = employee.map(employee=>{
+     
+//               return {
+//                  position: `${employee.position}`,
+//                   name: employee.name,
+//                   fullLastName: `${employee.lastname1} ${employee.lastname2}`,
+//                   email: employee.email 
+                 
+//               }
+//           })
+     
+//           console.log(dataUpdate);   
+//       }
+//       getBoss()

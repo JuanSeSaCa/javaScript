75ac651c858529abbd8f1ce85f90aabd0cbf1269
JuanSeSaCa <156488290+JuanSeSaCa@users.noreply.github.com>
2024-04-26 16:40:42 -0500

    feat: :construction: consultas 6 y 7
    se planteo el armado de la  estructura de la consulta 7, pero sigue en contruccion

--- js/module/request.js
+++ js/module/request.js
@@ -1,1 +1,21 @@
-//7. Devuelve un listado con los distintos estados por los que puede pasar un pedido.
+//7. Devuelve un listado con los distintos estados por los que 
+//puede pasar un pedido.
+
+
+export async function getRequestFullStatus(status) {
+    let res = await fetch(`http://localhost:3000/requests?status=${status}`);
+    let requests = await res.json();
+    let dataUpdate = new set (requests.map(status => {
+        return {
+            status:status.status
+        }
+    }));
+
+    console.log(dataUpdate);
+}
+getRequestFullStatus()
+
+
+
+
+

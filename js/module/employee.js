a20d651bb92c0d18f85167a87672a14116bea27f
JuanSeSaCa <156488290+JuanSeSaCa@users.noreply.github.com>
2024-05-30 10:23:04 -0500

    feat: :construction: consultas internas 8 y 9

--- js/module/employee.js
+++ js/module/employee.js
@@ -67,3 +67,51 @@ export async function getFullnameEmailEmployeesWithoutSales(position) {
     console.log(dataUpdate);
 }
 //getFullnameEmailEmployeesWithoutSales("Representante Ventas");
+
+// Consulta interna
+
+// 8. Devuelve un listado con el nombre de los empleados junto con el nombre de sus jefes.
+
+export const getAllEmployeesAndBossNames = async () => {
+    let res = await fetch('http://172.16.101.146:5542/employee').then(res => res.json());
+    let dataUpdate = res.map(async (val) => {
+        if (val.code_boss == null) {
+            return{
+                Empleado: val.name + ' ' + val.lastname1 + ' ' + val.lastname2,
+                JefeACargo: val.name + ' ' + val.lastname1 + ' ' + val.lastname2
+            };
+        }
+        let [boss] = await getEmployeesByCode(val.code_boss);
+        return {
+            Empleado: val.name + ' ' + val.lastname1 + ' ' + val.lastname2,
+            JefeACargo: boss.name + ' ' + boss.lastname1 + ' ' + boss.lastname2
+        };
+    });
+    return await Promise.all(dataUpdate);
+};
+
+// 9. Devuelve un listado que muestre el nombre de cada empleados, el nombre de su jefe y el nombre del jefe de sus jefe.
+export const getAllEmployeeBossAndHisBossNames = async () => {
+    let res = await fetch('http://172.16.101.146:5542/employee').then(res => res.json());
+    let dataUpdate = res.map(async (val) => {
+        if (val.code_boss == null) return {
+            Empleado: `${val.name} ${val.lastname1} ${val.lastname2}`,
+            JefeACargo: `${val.name} ${val.lastname1} ${val.lastname2}`,
+            JefeDeJefe: `${val.name} ${val.lastname1} ${val.lastname2}`
+        };
+        let [boss] = await getEmployeesByCode(val.code_boss);
+        if (boss.code_boss == null) return {
+            Empleado: `${val.name} ${val.lastname1} ${val.lastname2}`,
+            JefeACargo: `${boss.name} ${boss.lastname1} ${boss.lastname2}`,
+            JefeDeJefe: `${boss.name} ${boss.lastname1} ${boss.lastname2}`
+        };
+        let [bossBoss] = await getEmployeesByCode(boss.code_boss);
+        return {
+            Empleado: `${val.name} ${val.lastname1} ${val.lastname2}`,
+            JefeACargo: `${boss.name} ${boss.lastname1} ${boss.lastname2}`,
+            JefeDeJefe: `${bossBoss.name} ${bossBoss.lastname1} ${bossBoss.lastname2}`
+        };
+    });
+    return await Promise.all(dataUpdate);
+};
+

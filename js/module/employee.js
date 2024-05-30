5f8520bd14b0e49ed4a315d8cd55b7f4e07a2e78
JuanSeSaCa <156488290+JuanSeSaCa@users.noreply.github.com>
2024-05-30 12:23:17 -0500

    feat: :construction: consulta extrerna 6 y  7

--- js/module/employee.js
+++ js/module/employee.js
@@ -153,4 +153,54 @@ export const getAllemployeesHaveNotClients = async () => {
     }
     return dataUpdate;
 };
+// 6.2 Devuelve un listado que muestre solamente los empleados que no tienen un cliente asociado junto con los datos de la oficina donde trabajan.
 
+export const getAllemployeesHaveNotClientsWithOffices = async() => {
+    let resEmployees = await fetch("http://172.16.101.146:5582/employee");
+    let employees = await resEmployees.json();
+    let resClients = await fetch("http://172.16.101.146:5581/clients"); 
+    let clients = await resClients.json();
+    let resOffices = await fetch("http://172.16.101.146:5585/offices"); 
+    let offices = await resOffices.json();
+
+    let dataUpdate = [];
+    for (let i = 0; i < employees.length; i++) {
+        let employeeAssociated = false;
+        for (let j = 0; j < clients.length; j++) {
+            if (employees[i].employee_code === clients[j].code_employee_sales_manager) {
+                employeeAssociated = true;
+                break;
+            }
+        }
+        if (!employeeAssociated) {
+            let employee = employees[i];
+            let employeeUpdate = { ...employee };
+            // Buscar la oficina del empleado
+            let office = offices.find(office => office.code_office === employee.code_office);
+            if (office) {
+                // Agregar los datos de la oficina al empleado
+                let officeUpdate = { id_office: office.id, ...office };
+                dataUpdate.push({ ...employeeUpdate, ...officeUpdate });
+            } else {
+                console.log('No se encontró la oficina para el empleado con código de oficina: ${employee.code_office}');
+            }
+        }
+    }
+    return dataUpdate;
+}
+
+//7. Devuelve un listado que muestre los empleados que no tienen una oficina asociada y los que no tienen un cliente asociado.
+
+export const getAllEmployeesThatArentAssociatedWithAnyClientOrOffice = async () => {
+    let res = await fetch("http://172.16.101.146:5582/employee")
+    let data = await res.json();
+    let dataUpdate = [];
+    for (let i = 0; i < data.length; i++) {
+        let [client] = await getAllClientsByManagerCode(data[i].employee_code);
+        let [office] = await getOfficesByCode(data[i].code_office);
+        if (client === undefined && office === undefined) {
+            dataUpdate.push(data[i]);
+        }
+    }
+    return dataUpdate;
+}

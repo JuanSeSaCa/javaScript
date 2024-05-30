609ef6df03488d141d4fd4578527a51fdf4eca97
JuanSeSaCa <156488290+JuanSeSaCa@users.noreply.github.com>
2024-05-30 09:25:11 -0500

    feat: :construction: cons internas 2 y 3

--- js/module/clients.js
+++ js/module/clients.js
@@ -79,3 +79,43 @@ export const getAllClientNameAndSalesManager = async () => {
     }
     return data;
 }
+
+// 2. Muestra el nombre de los clientes que hayan realizado pagos junto con el nombre de sus representantes de ventas.
+
+export const getAllClientNameAndSalesManagerWithPayment = async () => {
+    let payment = await getPaymentByClientCode()
+    let dataUpdate = []
+    for (const val of payment) {
+        let res = await fetch(`http://172.16.101.146:5581/clients?client_code=${val.code_client}`);
+        let data = await res.json();
+        let [dataEmployee] = await getEmployeesByCode(data[0].code_employee_sales_manager)
+        if (!dataUpdate.some(elmt => elmt.Client_name == data[0].client_name)) {
+            let datos = ({
+                Client_name: data[0].client_name,
+                Client_Code: val.code_client,
+                Manager_name: `${dataEmployee.name} ${dataEmployee.lastname1} ${dataEmployee.lastname2}`,
+                Manager_Code: dataEmployee.employee_code
+            })
+            dataUpdate.push(datos)
+        }
+    }
+    return dataUpdate;
+}
+
+// 3. Muestra el nombre de los clientes que **no** hayan realizado pagos junto con el nombre de sus representantes de ventas.
+
+export const getAllClientNameAndSalesManagerWithoutPayment = async () => {
+    let res = await fetch("http://172.16.101.146:5581/clients").then(res => res.json());
+    let dataUpdate = [];
+    for (const val of res) {
+        let [employee] = await getEmployeesByCode(val.code_employee_sales_manager);
+        let [pago] = await getPaymentByClientCode(val.client_code)
+        if (pago == undefined) dataUpdate.push({
+            Client_name: val.client_name,
+            clientCode: val.client_code,
+            Manager_name: `${employee.name} ${employee.lastname1} ${employee.lastname2}`,
+            Manager_Code: val.code_employee_sales_manager
+        })
+    }
+    return dataUpdate;
+}
